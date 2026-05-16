8519d51696f301871170991ebbb01639de647105
Agatha França <agathafranca@outlook.com>
2026-05-16 10:00:00 -0300

    Upload local work

---
+++ js/app.js
@@ -0,0 +1,30 @@
+console.log('Olá, Javascript!')
+
+var userName = 'Agatha'
+
+document.getElementById('user-name').innerHTML = userName
+
+// Variáveis //
+
+// var nome = 'Mestre Yoda'
+// var idade = 100
+// var jedi = true
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+// console.log(typeof jedi)
+
+// Operadores matemáticos //
+
+var n1 = 5
+var n2 = 5
+
+console.log(typeof n1)
+console.log(typeof n2)
+
+// + para somar
+// - para subtrair
+// * para multiplicar
+// / para dividir
+
+var total = n1 + n2 
